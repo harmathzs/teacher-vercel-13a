b0fc5f4c16ca82d8150ab947ef13a797f78fceec
Zsolt Harmath <harmathzs@gmail.com>
2025-09-19 11:20:00 +0200

    Show teachers

--- react-app/src/Teachers.jsx
+++ react-app/src/Teachers.jsx
@@ -6,7 +6,12 @@ export default class Teachers extends React.Component {
 
   render() {
     return <div style={{margin: '20px'}}>
-      {JSON.stringify(this.state.teachers)}
+        <ol>
+            {this.state.teachers.map((teacher, idx)=><li key={idx}>
+                <b>Name: </b>{teacher.name}; <b>Class: </b>{teacher.hisClass}
+                {teacher.favorite && <span>; Our favorite teacher!</span>}
+            </li>)}
+        </ol>
     </div>
   }
 
