f59ee0ef5a1271a9276812504587088ee5ec5a73
Zsolt Harmath <harmathzs@gmail.com>
2025-09-19 10:52:42 +0200

    Implement GET /teachers

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
+var global = {
+    teachers: [
+        {
+            name: 'Gipsz Jakab',
+            hisClass: '9/A',
+            favorite: false,
+        },
+        {
+            name: 'Kolompár Dzsoki',
+            hisClass: '18/C',
+            favorite: true,
+        },        
+    ],
+    //favoriteTeacherIdx: null,
+}
+
 // local test: GET http://localhost:3333/teachers
 app.get('/teachers', (req, res)=>{
-    // TODO - implement GET /teachers
-    res.sendStatus(200)
+    res.status(200).json(global.teachers)
 })
 
 const port = 3333
