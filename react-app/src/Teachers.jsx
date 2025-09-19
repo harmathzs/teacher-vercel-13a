fd804f2426443cf986c840299fb758d6f1b2e745
Zsolt Harmath <harmathzs@gmail.com>
2025-09-19 11:12:56 +0200

    Retrieve response data successfully

--- react-app/src/Teachers.jsx
+++ react-app/src/Teachers.jsx
@@ -13,7 +13,11 @@ export default class Teachers extends React.Component {
   componentDidMount() {
     // callout for teachers data
     fetch('http://localhost:3333/teachers')
-    .then(console.log)
+    .then(data=>data.json())
+    .then(data=>{
+        console.log('got data: ', data)
+        this.setState({teachers: data})
+    })
     .catch(console.warn)
     .finally(()=>{
         console.log('fetch done')
