65a12c332a2cf46376532090da1b20bb8f70284c
Zsolt Harmath <harmathzs@gmail.com>
2025-09-19 11:52:41 +0200

    Log serverless function

--- react-app/api/teachers.js
+++ react-app/api/teachers.js
@@ -18,5 +18,7 @@ var global = {
 //export global
 
 export default async function handler(req, res) {
+    if (req.method == 'GET') console.log('GET /teachers serverless function starts', global)
+
     res.status(200).json(global.teachers)
 }
